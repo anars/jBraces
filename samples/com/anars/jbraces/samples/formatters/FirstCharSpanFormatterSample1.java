c999c8e814c4db2f095a774f52bf920448f20ebc
Kay Anar <github@anars.com>
2016-02-24 12:25:28 -0500

    FirstCharSpanFormatter sample added

--- samples/com/anars/jbraces/samples/formatters/FirstCharSpanFormatterSample1.java
+++ samples/com/anars/jbraces/samples/formatters/FirstCharSpanFormatterSample1.java
@@ -34,6 +34,7 @@ public class FirstCharSpanFormatterSample1
     template = templateProcessor.apply(template);
     System.out.println(template);
   }
+
   public static void main(String[] args)
   {
     new FirstCharSpanFormatterSample1();
